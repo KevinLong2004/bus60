bb6374e1b343bef57f8aa34bdc1de4d6bd3f6e11
“Kevin <kevinlo@student.chalmers.se>
2025-05-16 01:25:19 +0200

    remove

--- config.py
+++
@@ -1,7 +0,0 @@
-# config.py
-# This file contains the configuration for the application.
-
-
-client_id = 'z12S_uz1wRXpK7BiQqyWdYQ5EPAa'
-client_secret = '2AsIhzBFC9aDlLEb5GjAlfwLUY8a'
-api_key = 'ejEyU191ejF3UlhwSzdCaVFxeVdkWVE1RVBBYToyQXNJaHpCRkM5YURsTEViNUdqQWxmd0xVWThh'
